526c5c9f1f72f49f9811906e313fd5bdd29c1ee7
skyrunner360 <mathurrishi31@gmail.com>
2021-12-27 23:33:00 +0530

    Play and stop working, queue, skip, next song, loop not working

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const Discord = require("discord.js");
-const { Client, Intents } = require('discord.js');
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
-const DisTube = require("distube");
-const distube = new DisTube.default(client,
+const Discord = require("discord.js")
+// const { Client, Intents } = require('discord.js');
+const client = new Discord.Client(
+  // { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] }
+  );
+const DisTube = require("distube")
+const distube = new DisTube(client,
   {
-    searchSongs: 1,
-    emitNewSongOnly: true,
+    searchSongs: true,
+    emitNewSongOnly: false,
     // leaveOnFinish: true
-    
+    leaveOnStop: false,
+    youtubeDL: true,
+    updateYouTubeDL: true,
+    emptyCooldown: 30
 }
 );
 const { token } = require("./info.json");
-const prefix = "-";
+const prefix = '-';
 
 client.on("ready", () => {
-  console.log(`${client.user.tag} Has logged in`);
-});
-
-client.on('error', (channel, error) => {
-	console.error(error)
-	channel.send(`An error encoutered: ${error.slice(0, 1979)}`) // Discord limits 2000 characters in a message
+  console.log(`${client.user.tag} Has logged in`)
 })
 
-client.on("messageCreate", async (message) => {
+
+client.on("message", async (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -38,7 +39,7 @@ client.on("messageCreate", async (message) => {
           ? "All Queue"
           : "This Song"
         : "Off"
-    }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+    }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
 
   // DisTube event listeners, more in the documentation page
   distube
@@ -63,19 +64,24 @@ client.on("messageCreate", async (message) => {
     )
     // DisTubeOptions.searchSongs = true
     .on("searchResult", (message, result) => {
-      let i = 0;
+      let i = 0
       message.channel.send(
         `**Choose an option from below**\n${result
           .map(
             (song) => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``
           )
           .join("\n")}\n*Enter anything else or wait 30 seconds to cancel*`
-      );
+      )
+    })
+    .on("searchCancel",(message)=>message.channel.send(`Searching Cancelled`))
+    .on("error",(message,e)=>{
+      console.error(e);
+      message.channel.send("An error Occured: " + e);
     });
     if(["play","p"].includes(command)){
       if(!message.member.voice.channel) return message.channel.send(":kiss: uhh! Master Chief you should be in a voice channel to command me!");
       if(!args[0]) return message.channel.send(":kiss: Uhh! Master Chief you should tell me what to play!!");
-      distube.play(message,args.join(' '));      
+      distube.play(message,args.join(' '));
 
   }
   if(["stop","s"].includes(command)){
@@ -83,8 +89,35 @@ client.on("messageCreate", async (message) => {
       if(!message.member.voice.channel) return message.channel.send(":kiss: uhh! Master Chief you should be in a voice channel to command me!");
       if(bot.voice.channel != message.member.voice.channel) return message.channel.send(":kiss: Uhh! Master Chief you are not in the same voice channel as me. Please cum inn chief!");
       distube.stop(message);
-      message.channel.send(":kiss: I stopped the music as per your command Master Chief!")
+      message.channel.send(":kiss: I stopped the music as per your command Master Chief!");
+  }  
+  if(command == "resume"){
+    distube.resume(message);
+    message.channel.send(":kiss: I resumed the previous playing song as per your command Chief!");
   }
+  if(command == "playSkip"){
+    distube.playSkip(message, args.join(" "));
+    message.channel.send(":kiss: I skipped the current playing song and now playing the new song as per your command Chief!");
+  }
+  if(command == "pause"){
+    distube.pause(message);
+    message.channel.send(":kiss: I paused the current playing song as per your command Chief!");
+  }
+  if(["loop","repeat"].includes(command)){
+    let mode = distube.setRepeatMode(message,parseInt(args[0]));
+    mode = mode ? mode==2 ? "Repeat queue":"Repeat Song":"Off";
+    message.channel.send("Set repeat mode to: `"+mode+"`");
+  }
+  if(command=="queue"){
+    let queue = distube.getQueue(message);
+    message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
+        `**${id+1}**. [${song.name}](${song.url}) - \`${song.formattedDuration}\``
+    ).join("\n"));
+  }
+  distube.on("error",(message,e)=>{
+    console.error(e);
+    message.channel.send("An error Occured: " + e);
+  });
 });
 
-client.login(token);
+client.login(token)
