84244d6482429d3292003b57661844b5bde9666f
skyrunner360 <mathurrishi31@gmail.com>
2021-12-30 03:18:47 +0530

    Used Environment Variables

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ client.once('ready', async () => {
 });
 
 client.on('ready', function() {
-  client.user.setActivity(config.activity, { type: config.activityType });
+  client.user.setActivity(process.env.MESSAGE, { type: process.env.ACTIVITY });
 });
 
 client.once('reconnecting', () => {
@@ -96,4 +96,4 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
